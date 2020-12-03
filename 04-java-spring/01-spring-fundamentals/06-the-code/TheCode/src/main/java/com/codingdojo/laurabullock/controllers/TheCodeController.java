21c87186084069d1a8daa761a69f497ca8408165
Laura V. Bullock <laura.v.bullock@gmail.com>
2020-12-03 00:27:17 -0500

    Java Spring Fundamentals - The Code - Assignment 6-modified

--- 04-java-spring/01-spring-fundamentals/06-the-code/TheCode/src/main/java/com/codingdojo/laurabullock/controllers/TheCodeController.java
+++ 04-java-spring/01-spring-fundamentals/06-the-code/TheCode/src/main/java/com/codingdojo/laurabullock/controllers/TheCodeController.java
@@ -17,15 +17,17 @@ public class TheCodeController {
 		method.addAttribute("errors", errors);
 		return "index.jsp";
 	}
-		
+	
+	//Check the code against the word "bushido".  
 	@RequestMapping(path="/code", method=RequestMethod.POST)
 	public String code(@RequestParam(value="guess") String guess, RedirectAttributes redirectAttributes) {
 		System.out.println(guess);
+		//If it's valid, give access to the code route
 		if(guess.equals("bushido")) {
 			System.out.println("Access Granted!");
 			return "code.jsp";
 		}
-		
+		//otherwise redirect to root and display a flash error message.
 		else {
 			redirectAttributes.addFlashAttribute("errors", "Access Denied.  You must train harder!");
 			System.out.println("Incorrect guess.  Access Denied!");
@@ -35,3 +37,19 @@ public class TheCodeController {
 }
 
 
+
+/*Assignment: The Code - http://learn.codingdojo.com/m/145/6906/49961
+Sam Samurai wants you to make a page for him that is only accessible to people who know the code. If they don't know the code, then he wants to tell them to train harder. Those that do know the code should be able to view the code he has provided you in the wireframe below.
+
+
+Topics:
+● Error Messages
+
+Tasks:
+● Create a page where a user can submit a code
+
+● Check the code against the word "bushido"
+
+● If it's valid, give access to the code route
+
+● otherwise redirect to root and display a flash error message.*/
